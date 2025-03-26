0a8219c5787c3e7771e1497f8fd06f2cbef98dce
Priyanshuchauhan17 <priyanshuchauhan1104@gmail.com>
2025-03-27 01:48:03 +0530

    hi

--- script.js
+++ script.js
@@ -33,22 +33,21 @@ elems.forEach(function(n){
 }
 //swiper js start yah se
 
-function swiperAnimation() {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    centeredSlides: true,
-    spaceBetween:100,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
- 
-}
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 swiperAnimation()
 pagw4animantion();
